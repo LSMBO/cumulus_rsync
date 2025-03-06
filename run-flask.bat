a8a3537c5ac11f42aad597bbcb7f4b585b637866
AlexandreBurel <AlexandreBurel@users.noreply.github.com>
2025-03-06 16:09:30 +0100

    add waitress wsgi server

--- run-flask.bat
+++ run-flask.bat
@@ -1,3 +1,4 @@
 rem call cumulus_rsync\.venv\Scripts\activate.bat
 set CUMULUS_DEBUG=true
-flask --app cumulus_rsync run --host=0.0.0.0 --port 8800
+rem flask --app cumulus_rsync run --host=0.0.0.0 --port 8800
+python cumulus_rsync\cumulus_rsync_main.py
