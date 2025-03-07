f3e83145e2131b3ce731e605002f0b7afad94792
AlexandreBurel <AlexandreBurel@users.noreply.github.com>
2025-03-07 10:26:16 +0100

    remove commented tests

--- cumulus_rsync/test/test_01_utils.py
+++ cumulus_rsync/test/test_01_utils.py
@@ -35,8 +35,6 @@ import os
 import re
 import shutil
 
-utils.reset_configuration()
-
 def test_initialization():
     assert utils.STORAGE_HOST == "localhost"
     utils.initialize("cumulus_rsync/test/cumulus_rsync.conf")
@@ -88,9 +86,6 @@ def test_read_progress_file():
     assert file == "TP4806_Slot2-1_1_4818.d"
     assert progress == 94249694
 
-# def test_is_send_queue_empty():
-#     assert utils.is_send_queue_empty() == True
-
 def test_extract_from_settings():
     settings = {"job_id": "1", "job_dir": "Job_1", "owner": "test.user", "files": '["File1.raw","File2.raw","File3.raw"]', "local_files": '["File.fasta"]'}
     job_id, job_dir, owner, shared_files, local_files = utils.extract_from_settings(settings)
@@ -100,22 +95,7 @@ def test_extract_from_settings():
     assert len(shared_files) == 3
     assert len(local_files) == 1
 
-# def test_add_to_queue():
-#     nb = utils.add_to_queue(1, "Job_1", "test.user", ["cumulus_rsync/test/File1.raw","cumulus_rsync/test/File2.raw","cumulus_rsync/test/File3.raw"], ["cumulus_rsync/test/File.fasta"])
-#     assert nb == 4
-#     assert len(utils.SEND_QUEUE) == 5
-
-# def test_list_shared_files_in_queue():
-#     list = utils.list_shared_files_in_queue()
-#     assert len(list) == 3
-
-# def test_get_number_of_cancelled_file_transfers():
-#     assert utils.get_number_of_cancelled_file_transfers(1) == 5
-
 def test_get_progress_for_job():
-    # add a fake job
-    # utils.SEND_QUEUE.append([2, "test.user", "cumulus_rsync/test/TP4806_Slot2-1_1_4818.d", 16, "Job_2", 94249694 * 1.25])
-    # dict = utils.get_progress_for_job(2, "test.user")
     files = [["cumulus_rsync/test/TP4806_Slot2-1_1_4818.d", 94249694 * 1.25]]
     dict = utils.get_progress_for_job(2, files)
     assert dict["TP4806_Slot2-1_1_4818.d"] == 80
@@ -127,8 +107,3 @@ def test_delete_progress_file():
     utils.delete_progress_file()
     # test that file no longer exists
     assert os.path.isfile(utils.PROGRESS_FILE) == False
-
-# def test_cancel_job():
-#     nb = utils.cancel_job(1)
-#     assert nb == 5
-#     assert len(utils.CANCEL_QUEUE) == 1
