4eb53d99dd315486e01571a7add012f2a118d0eb
AlexandreBurel <AlexandreBurel@users.noreply.github.com>
2025-05-14 11:59:53 +0200

    added edit service

---
+++ service/edit-service.bat
@@ -0,0 +1,9 @@
+@echo off
+
+rem put the path of the current script in a variable and print it
+set script_path=%~dp0
+
+rem call nssm to start the service
+"%script_path%\nssm-2.24\win64\nssm.exe" edit "Cumulus RSync Agent"
+
+pause
